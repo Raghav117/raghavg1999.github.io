356bcffee2db2e0c874e9ee362a80089eee79865
Raghav Garg <42090624+Raghav117@users.noreply.github.com>
2021-10-08 10:17:03 +0530

    improvements

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,14 +6,14 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "fd28604a227bfbb6e2c648e9a7882411",
+"assets/NOTICES": "a64d1a3a0601623b9077d0f7792eb210",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "18297f2f8da3650ef599a60f9b316166",
-"/": "18297f2f8da3650ef599a60f9b316166",
-"main.dart.js": "6624b384b8c10977620161c4b8a53254",
+"index.html": "c44362993201aaa90b63d2145185b9c2",
+"/": "c44362993201aaa90b63d2145185b9c2",
+"main.dart.js": "8e79689785499ec906b0ace9b9d75085",
 "manifest.json": "db512dab4f7d0265465c982cdcd988a4",
 "version.json": "94c733edefcbfb701118d2e691335f11"
 };
